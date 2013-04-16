9a090e23ce329a2c28a6b20a9f8410bfaed60fd4
UNI <UNIMacBookPro@hachitani-masato-no-MacBook-Pro.local>
2013-04-16 18:09:33 +0900

    Initial Commit

---
+++ KTHttpRequest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KTHttpRequest
+//
+//  Created by UNI on 2013/04/16.
+//  Copyright (c) 2013年 UNI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KTHttpRequest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KTHttpRequest
+//
+//  Created by UNI on 2013/04/16.
+//  Copyright (c) 2013年 UNI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
